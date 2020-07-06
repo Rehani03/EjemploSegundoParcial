b694bc987ebfe5952ff501011d7f3d91974ae253
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-07-06 09:50:14 -0400

    Configurado..

--- EjemploSegundoParcial/Startup.cs
+++ EjemploSegundoParcial/Startup.cs
@@ -29,6 +29,7 @@ namespace EjemploSegundoParcial
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredToast();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
